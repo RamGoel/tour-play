5b7e6e4d1847b7e79f09592318ba446cab9c3309
Ram Goel <rgoel766@gmail.com>
2025-03-01 01:25:27 +0530

    fix: axios base url

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError } from "axios";
 
 export const API = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "/api",
 });
 
 export const parseAxiosMessage = (err: unknown) => {
